b8ded6ede2132018541ff6951a5ae1611de8df6f
tablenine <tablenine@naver.com>
2019-03-20 14:30:18 +0900

    Update startFlume.sh

--- Flume/startFlume.sh
+++ Flume/startFlume.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/root/flumeTest/flume/bin/flume-ng agent --conf conf --conf-file /root/flumeTest/flume/conf/example.conf --name a1 -Dflume.root.logger=INFO,console &
+/root/flumeTest/flume/bin/flume-ng agent --conf conf --conf-file /root/flumeTest/flume/conf/example.conf --name a1 -Dflume.root.logger=INFO,console --classpath "/opt/cloudera/parcels/CDH-6.1.1-1.cdh6.1.1.p0.875250/lib/hive-hcatalog/share/hcatalog/*:/opt/cloudera/parcels/CDH-6.1.1-1.cdh6.1.1.p0.875250/lib/hive/lib/* &
